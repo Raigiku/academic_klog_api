4c622ba281867f38a26f776f3d701c281cf5d0a9
Raigiku <raigiku@hotmail.com>
2021-04-13 15:25:10 -0500

    Update

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ async fn main() -> Result<(), ApiError> {
             .service(get_programs)
             .service(get_windows)
     })
-    .bind(format!("127.0.0.1:{}", port))?
+    .bind(format!("0.0.0.0:{}", port))?
     .run()
     .await?;
 
